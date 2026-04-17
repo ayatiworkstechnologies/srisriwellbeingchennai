b9c7fc06bc70af9bd40cda4a731c4d810fcfefe1
rubankumarsankar <srirubankumar@gmail.com>
2026-04-17 12:45:44 +0530

    17/04/2026 update gtag

--- app/layout.js
+++ app/layout.js
@@ -27,24 +27,25 @@ export default function RootLayout({ children }) {
     <html lang="en" className={`${cocogoose.variable} ${helmet.variable}`}>
       <body>
         <Script
-          id="google-tag-manager"
+          id="google-tag"
+          src="https://www.googletagmanager.com/gtag/js?id=G-KR56VJ4D2M"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-tag-config"
           strategy="afterInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PCQPKFM3');`,
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-KR56VJ4D2M', {
+  send_page_view: true,
+  page_path: window.location.pathname,
+  cookie_flags: 'SameSite=None;Secure'
+});`,
           }}
         />
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PCQPKFM3"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
         <Header />
         <main>{children}</main>
         <Footer />
